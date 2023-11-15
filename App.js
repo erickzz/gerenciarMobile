3439515f382ef3f2c12c5cd4bcd2ee870ee397a1
erickreis <erick.reis2@gmail.com>
2023-11-15 12:24:40 -0300

    second commit

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet } from 'react-native';
+import RootNavigation from './navigation/RootNavigation';
+import { AuthContextProvider } from './context/authContext';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthContextProvider>
+      <NavigationContainer>
+        <RootNavigation />
+      </NavigationContainer>
+    </AuthContextProvider>
   );
 }
 
